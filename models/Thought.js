123b0b03ea55042e4f7dca6a6c166eea20bfc711
Bryan Tempini <mistatempini@gmail.com>
2023-01-07 08:21:48 -0500

    schema update thought/user

--- models/Thought.js
+++ models/Thought.js
@@ -6,9 +6,12 @@ const thoughtSchema = new Schema(
       type: String,
       required: true,
       //validation between 1 - 280 characters
+      minlength: 1,
+      maxlength: 280,
     },
     createdAt: {
       type: { type: Date, default: Date.now },
+      //need to get format date
     },
     username: {
       type: String,
