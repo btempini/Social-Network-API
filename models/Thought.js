1dd99d1d674bac70f2370dc41516617b35062361
Bryan Tempini <mistatempini@gmail.com>
2023-01-07 08:08:00 -0500

    schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,12 +1,25 @@
 const { Schema, model } = require("mongoose");
 
-const thoughtSchema = new Schema ({
-  thoughtText: {
-    type: String,
-    required: true,
-    //validation between 1 - 280 characters
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      //validation between 1 - 280 characters
+    },
+    createdAt: {
+      type: { type: Date, default: Date.now },
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reaction: [reactionSchema],
   },
-  createdAt: {
-    type: 
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
   }
-})
+);
