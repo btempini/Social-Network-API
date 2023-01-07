264c877d7ad6d0be1c24c82097bb5146010df198
Bryan Tempini <mistatempini@gmail.com>
2023-01-07 11:32:18 -0500

    adding user controls and routes, testing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,33 @@
+const { restart } = require("nodemon");
 const { User, Thought } = require("../models");
 
 const userController = {
+  //Create a User
   createUser(req, res) {
-    console.log("working");
-    User.create(req.body).then((databaseUserData) => {
-      console.log(databaseUserData);
-      res.json(databaseUserData);
+    User.create(req.body)
+      .then((dbUserData) => {
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  //Get all users
+  getAllUsers(req, res) {
+    User.find().then((dbUserData) => {
+      res.json(dbUserData);
+    });
+  },
+  //Get user by ID
+  getUserById(req, res) {
+    User.findOne({ _id: req.params._id }).then((dbUserData) => {
+      if (!dbUserData) {
+        return res
+          .status(404)
+          .json({ message: "No user with this id in database!" });
+      }
+      res.json(dbUserData);
     });
   },
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
-const { createUser } = require("../../controllers/userController");
+const {
+  createUser,
+  getAllUsers,
+  getUserById,
+} = require("../../controllers/userController");
 
-//localhost:27017/api/users
-router.route("/").post(createUser);
+//localhost:3001/api/users
+router.route("/").get(getAllUsers).post(createUser);
+
+//localhost:3001/api/users/:_id
+router.route("/:_id").get(getUserById);
 
 module.exports = router;
